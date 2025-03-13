341de2a3396ef3a864adeddb38a8b8ffd8b52469
urbanxtreme <skrhari2020@gmail.com>
2025-03-13 13:06:24 +0530

    ok

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -10,7 +10,7 @@ const Navigation = () => {
   const navItems = [
     { name: "Home", path: "/" },
     { name: "Events", path: "/events" },
-    { name: "Gallery", path: "/gallery" },
+    // { name: "Gallery", path: "/gallery" },
     { name: "Interactive Map", path: "/map" },
     { name: "About & Contact", path: "/about" },
     { name: "Registeration", path: "/event-registration" },
