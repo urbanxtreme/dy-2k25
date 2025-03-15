9910481245b0365958018b82b2c5cf66237c6f6b
urbanxtreme <skrhari2020@gmail.com>
2025-03-15 21:25:40 +0530

    oknewww

--- src/App.tsx
+++ src/App.tsx
@@ -22,10 +22,10 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/dy-2k25">
         <Routes>
           <Route path="/login" element={<LogMeinDaddy />} />
-          <Route path="/ambi" element={<Campus />}/>
+          <Route path="/ambi" element={<Campus />} />
           <Route
             path="*"
             element={
@@ -38,9 +38,15 @@ const App = () => (
                     <Route path="/gallery" element={<GalleryPage />} />
                     <Route path="/map" element={<MapPage />} />
                     <Route path="/about" element={<AboutPage />} />
+                    <Route
+                      path="/event-registration/:id"
+                      element={<EventRegistration />}
+                    />
+                    <Route
+                      path="/event-registration"
+                      element={<EventRegistration />}
+                    />
                     <Route path="*" element={<NotFound />} />
-                    <Route path="/event-registration/:id" element={<EventRegistration />} />
-                    <Route path="/event-registration" element={<EventRegistration />} />
                   </Routes>
                 </main>
                 <Footer />
@@ -54,21 +60,3 @@ const App = () => (
 );
 
 export default App;
-const urlMapping = {
-  1: "https://link.dakshayanthra.in/MixDY",
-  2: "https://link.dakshayanthra.in/LACouture",
-  3: "https://link.dakshayanthra.in/Bailamo",
-  4: "https://link.dakshayanthra.in/Euphony",
-  5: "https://link.dakshayanthra.in/Bloomer",
-  6: "https://link.dakshayanthra.in/BeatTheSpot",
-  7: "https://link.dakshayanthra.in/Groove",
-  8: "https://link.dakshayanthra.in/ElDueto",
-  9: "https://example.com/9",
-  10: "https://example.com/10",
-  11: "https://link.dakshayanthra.in/CA",
-  12: "https://example.com/12",
-  13: "https://docs.google.com/forms/d/e/1FAIpQLSedvBzR7NXWCwOaa5P3cpWw-HKoW_G85lrm7aBspJeaKOiZ9g/viewform?usp=dialog",
-  14: "https://docs.google.com/forms/d/e/1FAIpQLSdWPALiPTVn0Nj-xhKEK11V1GbC20HtYo26tKv7rUwxfAyvnw/viewform?usp=header",
-  15: "https://docs.google.com/forms/d/e/1FAIpQLSfhYGwsRkw1T2vzNf6AM_GOnA1rPh02ReCj8thTqWRPX5eO-w/viewform?usp=header",
-  16: "https://example.com/16",
-};
